42c53284898519b44d5480021cafd239b1f98780
dwalczak97 <d.melon@op.pl>
2024-06-29 17:28:44 +0200

    task 2

--- task-2.js
+++ task-2.js
@@ -1,5 +1,5 @@
 const getUsersWithFriend = (users, friendName) =>
-    users.filter(user.friends => user.friends.includes(friendName));
+  users.filter((user) => user.friends.includes(friendName));
 
 const allUsers = [
   {
